0f03e47ed94ef776296b325c7b953dee5b0c5c9f
mjmontgomery-sjsu <42755215+mjmontgomery-sjsu@users.noreply.github.com>
2019-10-24 16:39:55 -0700

    created constructor

--- src/Flashcard.java
+++ src/Flashcard.java
@@ -1,8 +1,19 @@
 /**
- *
+ * Stores and edits information for the questions and answers
  */
 public class Flashcard {
 
-    
+    public Flashcard(string question, string answer){
+        this.question = question;
+        this.answer = answer;
+    }
+
+
+
+
+    private string question;
+    private string answer;
+
+
 
 }
