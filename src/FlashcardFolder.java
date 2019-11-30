532a499bbfd6f6621f4c54b21b9ddd772513bf55
mjmontgomery-sjsu <42755215+mjmontgomery-sjsu@users.noreply.github.com>
2019-11-30 05:15:34 -0800

    Added Variables to FlashcardFolder

--- src/FlashcardFolder.java
+++ src/FlashcardFolder.java
@@ -1,8 +1,18 @@
+import java.util.LinkedList;
+
 /**
- * Stores Decks of Flashcards
+ * Stores Decks of Flashcards and is the model for the interface of the
  */
 public class FlashcardFolder {
 
+    private Deck deck;
+    private LinkedList<Flashcard> deckOfCards;
+    private static int counterForCards;
+    private Flashcard current;
 
+    public FlashcardFolder()
+    {
+
+    }
 
 }
